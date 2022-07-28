2a665de056441433903e5ed69adfbe9eca874596
Miguel Rodriguez <zeromatrix75@gmail.com>
2022-07-27 23:07:04 -0500

    Scenes now switchable within game.

--- Mystery Project2/src/application/controller/SceneController.java
+++ Mystery Project2/src/application/controller/SceneController.java
@@ -3,6 +3,7 @@ package application.controller;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -13,42 +14,48 @@ public class SceneController {
 
 		private Stage stage;
 		private Scene scene;
-//		private Parent root;
+		private Parent root;
 		
+		@FXML
 		public void switchToMenu(ActionEvent event) throws IOException {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/view/MainMenu.fxml"));
+		    root = FXMLLoader.load(getClass().getResource("/application/view/MainMenu.fxml"));
 			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();
 		}
 		
-		
+		@FXML
 		public void switchToEntrance(ActionEvent event) throws IOException {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/view/HomeEntrance.fxml"));
+			root = FXMLLoader.load(getClass().getResource("/application/view/HomeEntrance.fxml"));
 			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
+			stage.setScene(scene);
 			stage.show();
 		}
 		
+		@FXML
 		public void switchToKitchen(ActionEvent event) throws IOException {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/view/Kitchen.fxml"));
+			root = FXMLLoader.load(getClass().getResource("/application/view/Kitchen.fxml"));
 			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
+			stage.setScene(scene);
 			stage.show();
 		}
 		
 		public void switchToBedroom1(ActionEvent event) throws IOException {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/view/Bedroom1.fxml"));
+			root = FXMLLoader.load(getClass().getResource("/application/view/Bedroom1.fxml"));
 			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
+			stage.setScene(scene);
 			stage.show();
 		}
 		
 		public void switchToBedroom2(ActionEvent event) throws IOException {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/view/Bedroom2.fxml"));
+			root = FXMLLoader.load(getClass().getResource("/application/view/Bedroom2.fxml"));
 			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
+			stage.setScene(scene);
 			stage.show();
 		}
 		
