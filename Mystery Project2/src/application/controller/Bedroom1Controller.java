3df6fce2c778558e44700e35c6928f51c00ce493
erikjh <erikmowgli22@gmail.com>
2022-08-04 17:27:45 -0500

    Added sound effects when going to different rooms, such as door opening and footsteps.

--- Mystery Project2/src/application/controller/Bedroom1Controller.java
+++ Mystery Project2/src/application/controller/Bedroom1Controller.java
@@ -1,12 +1,21 @@
 package application.controller;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import javafx.event.ActionEvent;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
 
 public class Bedroom1Controller extends AbstractSceneSwitchController {
 	public void switchToEntrance(ActionEvent event) throws IOException {
 		super.sceneSwitcher(event, "/application/view/HomeEntrance.fxml");
+		
+		// open door sound effect
+		String musicFile = "Audio/dooropen.mp3";
+		Media media = new Media(Paths.get(musicFile).toUri().toString());
+		AudioClip doorOpen = new AudioClip(media.getSource());
+		doorOpen.play();
 	}
 	
 
--- Mystery Project2/src/application/controller/KitchenController.java
+++ Mystery Project2/src/application/controller/KitchenController.java
@@ -1,12 +1,21 @@
 package application.controller;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import javafx.event.ActionEvent;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
 
 public class KitchenController extends AbstractSceneSwitchController {
 	public void switchToEntrance(ActionEvent event) throws IOException {
 		super.sceneSwitcher(event, "/application/view/HomeEntrance.fxml");
+		
+		// footsteps to kitchen sound effect
+		String musicFile = "Audio/footsteps.mp3";
+		Media media = new Media(Paths.get(musicFile).toUri().toString());
+		AudioClip footstepsKitchen = new AudioClip(media.getSource());
+		footstepsKitchen.play();
 	}
 
 }
